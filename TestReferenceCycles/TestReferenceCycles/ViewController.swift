cf09255ae5aa1f594134b6601e81a52495bde8b4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-05-15 20:44:52 +0900

    PersonクラスとApartmentクラスの記述

---
+++ TestReferenceCycles/TestReferenceCycles/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TestReferenceCycles
+//
+//  Created by home on 2018/05/15.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    var apartment: Apartment?
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+class Apartment {
+    let unit: String
+    init(unit: String) {
+        self.unit = unit
+    }
+    var tenant: Person?
+    deinit {
+        print("Apartment \(unit) is being deinitialized")
+        
+    }
+}
+
